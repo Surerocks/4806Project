bc9dfb870de0e7d854980608f180b3abc40c38ae
Marc teBoekhorst <marctebo@gmail.com>
2017-03-16 12:17:19 -0400

    Added more test cases to ProjectTest

--- src/main/java/tests/ProjectTest.java
+++ src/main/java/tests/ProjectTest.java
@@ -1,10 +1,11 @@
 package tests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import suhdude.Professor;
 import suhdude.Project;
 
 /** 
@@ -21,7 +22,50 @@ public class ProjectTest {
 
 	@Test
 	public void testProject() {
+		String name = "project";
+		pro = new Project(name);
+		pro.setMaxStudents(4);
+		pro.setProf(new Professor("prof","profemail"));
 		
+		assertTrue("Project information incorrect",
+				pro.getName().equals("project") 
+				&& pro.getMaxStudents()==4
+				&& pro.getProf().getName().equals("prof")
+				&& pro.getProf().getEmail().equals("profemail"));
 	}
 
+	@Test
+	public void testGetId(){
+		int id = 1;
+		pro = new Project("project");
+		pro.setId(id);
+		assertTrue("Project id not correct", pro.getId()==1);
+	}
+	
+	@Test
+	public void testSetId(){
+		int id = 1;
+		pro = new Project("project");
+		assertTrue("Project id is not correct", pro.getId()==0);
+		pro.setId(id);
+		assertTrue("Project id is not correct", pro.getId()==1);
+	}
+	
+	@Test
+	public void testGetName(){
+		String name = "project";
+		pro = new Project(name);
+		assertTrue("Project name is not correct", pro.getName().equals("project"));
+	}
+	
+	@Test
+	public void testSetName(){
+		String name = "project";
+		pro = new Project(name);
+		assertTrue("Project name is not correct", pro.getName().equals("project"));
+		name = "project2";
+		pro.setName(name);
+		assertTrue("Project name is not correct", pro.getName().equals("project2"));
+		
+	}
 }
